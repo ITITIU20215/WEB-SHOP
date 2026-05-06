b0dafcac4780d86a93c697044872caa30b04708a
Nguyễn Vạn Huy <91870430+ITITIU20215@users.noreply.github.com>
2026-05-07 05:10:22 +0700

    Add files via upload

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getCartInfo,
+  addToCart,
+  removeFromCart,
+  addItem,
+  removeItem,
+  modifyCart,
+} = require("../controllers/cartControllers.js");
+
+router.post("/add", addToCart);
+
+router.post("/add-item", addItem);
+
+router.post("/remove", removeFromCart);
+
+router.get("/:user_id", getCartInfo);
+
+router.post("/remove-item", removeItem);
+
+router.post("/modify", modifyCart);
+
+module.exports = router;
